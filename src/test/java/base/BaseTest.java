a46a5c03e65a702a3900558ed83f2b06de8dd0e7
Nik <nikpl2018@users.noreply.github.com>
2017-05-31 22:07:07 +0300

    added menu test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,6 +5,9 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.HashMap;
@@ -12,27 +15,45 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
+
+    private static final String DRIVERS_PATH = System.getProperty("user.dir") + "/src/test/resources/webdrivers";
+
     protected WebDriver wd;
 
     @Before
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/test/resources/webdrivers/chromedriver.exe");
-        Map<String, Object> chromePrefs = new HashMap<>();
-        chromePrefs.put("profile.default_content_settings.popups", 0);
-        chromePrefs.put("credentials_enable_service", false);
-        chromePrefs.put("disable-popup-blocking", "true");
-        chromePrefs.put("download.prompt_for_download", "false");
-        chromePrefs.put("profile.default_content_setting_values.notifications", 2);
-        chromePrefs.put("directory_upgrade", true);
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("prefs", chromePrefs);
-        options.addArguments("no-sandbox");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-print-preview");
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        wd = new ChromeDriver(capabilities);
-        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd = wdFactory(BrowserType.CHROME);
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    private WebDriver wdFactory(String browser) {
+        if (BrowserType.CHROME.equals(browser)) {
+            System.setProperty("webdriver.chrome.driver", DRIVERS_PATH+"/chromedriver.exe");
+            Map<String, Object> chromePrefs = new HashMap<>();
+            chromePrefs.put("profile.default_content_settings.popups", 0);
+            chromePrefs.put("credentials_enable_service", false);
+            chromePrefs.put("disable-popup-blocking", "true");
+            chromePrefs.put("download.prompt_for_download", "false");
+            chromePrefs.put("profile.default_content_setting_values.notifications", 2);
+            chromePrefs.put("directory_upgrade", true);
+            ChromeOptions options = new ChromeOptions();
+            options.setExperimentalOption("prefs", chromePrefs);
+            options.addArguments("no-sandbox");
+            options.addArguments("disable-infobars");
+            options.addArguments("--disable-print-preview");
+            options.addArguments("--start-maximized");
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+            return new ChromeDriver(capabilities);
+        } else if (BrowserType.FIREFOX.equals(browser)) {
+            System.setProperty("webdriver.gecko.driver",DRIVERS_PATH+"/geckodriver.exe");
+            return new FirefoxDriver();
+        } else if (BrowserType.IE.equals(browser)) {
+            System.setProperty("webdriver.ie.driver",DRIVERS_PATH+"/IEDriverServer.exe");
+            return new InternetExplorerDriver();
+        } else {
+            throw new IllegalArgumentException("Неподдерживаемый браузер: " + browser);
+        }
     }
 
     @After
---
+++ src/test/java/litecart/admin/ClickAllMenu.java
@@ -0,0 +1,54 @@
+package litecart.admin;
+
+import base.BaseTest;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClickAllMenu extends BaseTest {
+
+    @Test
+    public void clickOnMenu() {
+        wd.get("http://localhost/litecart/admin/");
+        login("admin", "admin");
+        getMenuItemNames().stream()
+                .peek(this::clickOnMenuItem)
+                .peek(name -> checkH1Present())
+                .flatMap(name -> getSubMenuItemNames().stream())
+                .peek(this::clickOnSubMenuItem)
+                .forEach(name -> checkH1Present());
+        WebElement logout = wd.findElement(By.xpath("//a[@title='Logout']"));
+        logout.click();
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.xpath("//input[@name='username']")).sendKeys(username);
+        wd.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+        wd.findElement(By.xpath("//button[@name='login']")).click();
+    }
+
+    private List<String> getMenuItemNames() {
+        return wd.findElements(By.cssSelector("ul#box-apps-menu > li")).stream().map(WebElement::getText).collect(Collectors.toList());
+    }
+
+    private void clickOnMenuItem(String itemName) {
+        wd.findElement(By.xpath(String.format("//ul[@id='box-apps-menu']/li[contains(., '%s')]", itemName))).click();
+    }
+
+    private List<String> getSubMenuItemNames() {
+        return wd.findElements(By.cssSelector("ul.docs > li")).stream().map(WebElement::getText).collect(Collectors.toList());
+    }
+
+    private void clickOnSubMenuItem(String itemName) {
+        wd.findElement(By.xpath(String.format("//ul[@class='docs']/li[contains(., '%s')]", itemName))).click();
+    }
+
+    private void checkH1Present() {
+        wd.findElement(By.tagName("h1"));
+    }
+
+}
